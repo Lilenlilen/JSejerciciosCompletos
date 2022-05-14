1d9e668fda58843f07d13ce7f020614f8e456aa0
Lilen Campodonico <campodonicolilen1@gmail.com>
2022-05-14 18:22:44 +0200

    resuelto ejeercicio bucles, falta realizar el extra

---
+++ 3-Bucles/script.js
@@ -0,0 +1,12 @@
+"use strict";
+// for (let hora = 8; hora < 23; hora++) {
+//     const cucus = "cucú ";
+//     console.log(`${hora} ${cucus.repeat(hora)}`);
+//   }
+
+// for (let hora = 0; hora < 24; hora++) {
+//   if (hora >= 8 && hora <= 22) {
+//     const cucus = "cucú ";
+//     console.log(`${hora} ${cucus.repeat(hora)}`);
+//   }
+// }
