ba52485c558da526d266a4ebe2d1f6f0f6c7860d
Lilen Campodonico <campodonicolilen1@gmail.com>
2022-05-14 15:44:52 +0200

    resuelto bucles anidados

--- 1-bucles-anidados/script.js
+++ 1-bucles-anidados/script.js
@@ -8,3 +8,10 @@ const catalogo = [
   "barbacoa",
   "tropical",
 ];
+
+for (let i = 0; i < catalogo.length; i++) {
+  for (let a = i + 1; a < catalogo.length; a++) {
+    const combinaciones = `${catalogo[i]} y ${catalogo[a]}`;
+    console.log(combinaciones);
+  }
+}
